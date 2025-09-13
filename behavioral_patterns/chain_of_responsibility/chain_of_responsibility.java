e63754118daad6d231e2bcd1365f9f3762c96214
nickzuck <nikhil1945.singh@gmail.com>
2025-09-13 21:23:30 +0530

    Update chain of responsibilty to create dynamic handler

--- behavioral_patterns/chain_of_responsibility/chain_of_responsibility.java
+++ behavioral_patterns/chain_of_responsibility/chain_of_responsibility.java
@@ -7,8 +7,13 @@ change in the denominations.
 
 Solution : We will use Chain of Responsibility to solve this problem. We will have handlers which will disburse the
 amount it can and pass the rest of the amount to the next handler.
+
+For the followup on creating a dynamic handler, we are going to use a chain builder class that reads config and writes
+the handlers in that order. That way handlers stay dumb - they don't care about what comes next
  */
 import java.lang.Override ;
+import java.util.List ;
+import java.lang.IllegalArgumentException;
 
 // This is the interface which will be used accross the concrete handlers
 interface Handler {
@@ -41,7 +46,6 @@ class TwoThousandNotesDispenser extends BaseHandler {
     }
 }
 
-
 // Concreate handler for 500 notes
 class FiveHunderNotesDispenser extends BaseHandler {
     public void handle(int amount){
@@ -57,7 +61,7 @@ class FiveHunderNotesDispenser extends BaseHandler {
 }
 
 // Concerete handler for 100 notes
-class HunderNotesDispenser extends  BaseHandler{
+class HunderNotesDispenser extends BaseHandler{
     public void handle(int amount){
         int notes = amount/100 ;
         if (notes > 0){
@@ -70,17 +74,43 @@ class HunderNotesDispenser extends  BaseHandler{
     }
 }
 
+class ChainBuilder {
+    public static Handler createHandler(int denom) {
+        return switch (denom){
+            case 2000 -> new TwoThousandNotesDispenser() ;
+            case 500 -> new FiveHunderNotesDispenser() ;
+            case 100 -> new HunderNotesDispenser() ;
+            default -> throw new IllegalArgumentException("Unsupported denomination : " + denom) ;
+        };
+    }
+
+    public static Handler buildChain(List<Integer> denominations) {
+        Handler head = null ;
+        Handler curr = null ;
+
+        for(int denom : denominations){
+            Handler handler = createHandler(denom) ;
+
+            if(head == null){
+                head = handler ;
+                curr = handler ;
+                continue;
+            }
+            curr.setNext(handler);
+            curr = handler ;
+        }
+
+        return head ;
+    }
+}
+
 class chain_of_responsibility{
     public static void main(String[] args){
-        Handler twoThoundsandHandler = new TwoThousandNotesDispenser() ;
-        Handler fiveHundredHandler = new FiveHunderNotesDispenser() ;
-        Handler hundredNotesHandler = new HunderNotesDispenser() ;
-
-        twoThoundsandHandler.setNext(fiveHundredHandler) ;
-        fiveHundredHandler.setNext(hundredNotesHandler) ;
+        List<Integer> denominations = List.of(2000, 500, 100) ;
+        Handler handler = ChainBuilder.buildChain(denominations);
 
         int amount = 3700 ;
 
-        twoThoundsandHandler.handle(amount) ;
+        handler.handle(amount) ;
     }
 }
